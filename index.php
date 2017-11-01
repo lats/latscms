e6cf8e379e18b8de7664da2caa96dd5f299352fb
Lats <galen12382@gmail.com>
2017-11-01 12:21:31 +0000

    modified:   index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ $html_body = NULL;
 $html_head = '
 <html>
 <head>
-  <title>Squire of Elements</title>
+  <title>YOUR TITLE HERE</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M\" crossorigin=\"anonymous">
   <script src="includes/list.js"></script>
 </head>
