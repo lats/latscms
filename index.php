6d7eaa885ff40c1faf0c3ce2d14828ebfd3af626
Bill <aardvark@zzyzx.info>
2019-07-10 08:34:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -32,11 +32,12 @@ $divs = array();
 $parse = new Parsedown();
 $path = "./pages/";
 $dirs = preg_grep('/^([^.])/', scandir($path));
+$pageTitle = "Your Awesome Page Title Here!";
 $html_body = NULL;
 $html_head = '
 <html>
 <head>
-  <title>Squire of Elements</title>
+  <title>' . $pageTitle  . '</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M\" crossorigin=\"anonymous">
   <link rel="stylesheet" href="main.css">
   <script src="includes/list.js"></script>
